e4d71820a4c73a07cef49821c9a4fc43041450e4
Augustin-FL <Augustin-FL@users.noreply.github.com>
2022-11-03 17:46:40 +0100

    Require pythonwhois-alt to be >=2.4.6 This fixes the issue with .fr parsing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='abuse_finder',
-      version='0.2',
+      version='0.3',
       description='Look for abuse contacts for IP, domain names, email addresses and URLs.',
       url='https://github.com/certsocietegenerale/abuse_finder',
       author='CERT Société Générale',
@@ -12,7 +12,7 @@ setup(name='abuse_finder',
       install_requires=[
         'ipwhois',
         'ipaddress',
-        'pythonwhois-alt',
+        'pythonwhois-alt>=2.4.6',
         'tldextract',
         'future',
         'dnspython'
